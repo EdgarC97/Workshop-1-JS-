8faa7b9ea8dbbba257dfe90185e2521dcd3cd50f
EdgarC97 <ecardona742@gmail.com>
2024-04-19 15:01:55 -0500

    add index.html and index.js file for workshop1

---
+++ index.js
@@ -0,0 +1,39 @@
+// Ejercicio 2:
+alert("¡Bienvenido al taller de JavaScript!");
+
+// Ejercicio 4:
+/**Se planea realizar primer workshop de Java
+Script para mejorar nuestras habilidades*/
+
+// Ejercicio 5:
+let miNumero = 10;
+console.log(miNumero);
+// Ejercicio 6:
+let miBooleano = true;
+console.log(typeof miBooleano);
+// Ejercicio 7:
+let miCadena = "Hola mundo";
+console.log(miCadena.length);
+// Ejercicio 8:
+let miArray = ["Hola", 742, [1, 2, 3]];
+console.log(miArray);
+// Ejercicio 9:
+let miObjeto = {
+  name: "edgar",
+  edad: 27,
+  ciudad: "Barranquilla",
+};
+console.log(miObjeto);
+// Ejercicio 10:
+let nombreUsuario = prompt("Ingresa tu nombre");
+alert(`Hola ${nombreUsuario}`);
+// Ejercicio 11:
+let gustoJs = confirm("¿Te gusta Java Script?");
+console.log("Me alegra saberlo");
+// Ejercicio 12:
+console.warn("Esto es una prueba");
+// Ejercicio 13:
+console.error("Esto es un fallo");
+// Ejercicio 14:
+let tablePractice = [1, 2, 3, 4, 5];
+console.table(tablePractice);
